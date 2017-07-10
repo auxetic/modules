c403a89b0d77fccad7812d8e6dedfe71792f244b
liuxu <liuxu89@mail.ustc.edu.cn>
2017-07-10 13:29:51 +0800

    add module mo_math

---
+++ 91-math.f90
@@ -0,0 +1,25 @@
+module mo_math
+    implicit none
+
+contains
+
+    function randperm(n) result( redata )
+        use ifport
+        implicit none
+        integer, intent(in) :: n
+        integer, dimension(n) :: redata
+        integer :: i, itemp
+        redata(1) = 1
+        do i=2, n
+            itemp = floor(rand(0)*i) + 1
+            if ( i /= itemp )
+                redata(i) = redata(itemp)
+                redata(itemp) = i
+            else
+                redata(i) = i
+            end if
+        end do
+    end function randperm
+
+end module
+
